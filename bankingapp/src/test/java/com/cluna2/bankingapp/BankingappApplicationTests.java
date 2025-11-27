ef7b859f2218db683396ddf73bf86ddd28298d42
cluna2 <lunacris2739@gmail.com>
2025-11-26 20:39:12 -0800

    Initial commit

---
+++ bankingapp/src/test/java/com/cluna2/bankingapp/BankingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cluna2.bankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
